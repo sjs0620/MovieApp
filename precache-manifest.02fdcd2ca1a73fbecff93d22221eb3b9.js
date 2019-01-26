0b315a12451040d30609d51c2c292bf2bbdc9849
sjs0620 <sjslsm@gmail.com>
2019-01-26 22:55:18 +0900

    Updates

---
+++ precache-manifest.02fdcd2ca1a73fbecff93d22221eb3b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7d10bba8c01b9f3d022",
+    "url": "/movieapp/static/js/runtime~main.e7d10bba.js"
+  },
+  {
+    "revision": "54564e046eb4bd40d4f2",
+    "url": "/movieapp/static/js/main.54564e04.chunk.js"
+  },
+  {
+    "revision": "22fdec2062f68dfa3e16",
+    "url": "/movieapp/static/js/1.22fdec20.chunk.js"
+  },
+  {
+    "revision": "54564e046eb4bd40d4f2",
+    "url": "/movieapp/static/css/main.9981e029.chunk.css"
+  },
+  {
+    "revision": "e2a54228ab10eeee4253abcc4403204f",
+    "url": "/movieapp/index.html"
+  }
+];
